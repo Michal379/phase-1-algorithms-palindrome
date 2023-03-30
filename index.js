06b38eddbb786d94ab1e95ccf6293d4225cee3aa
Michal <khakaimic4@gmail.com>
2023-03-30 16:24:15 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
+const { expect } = require("chai");
+
 function isPalindrome(word) {
   // Write your algorithm here
-}
+// const wordArray=["abba","a","racecar","robot",""]
+//   if (word===isPalindrome){
+//     return true
+//   }else{
+// return false
+//   }
+// }
 
+const reversedWord=reverse(word)
+if (word===reversedWord){
+  return true
+}else{
+  return false
+}
+function reverse(word){
+  const wordArray=word.split("");
+  const reversedWordArray=wordArray.reverse();
+  const reversedWord=reversedWordArray.join("");
+  return reversedWord;
+}
+}
 /* 
   Add your pseudocode here
-*/
-
+sis=sis===isPalindrome//true
+brother=rethorb===isPalindrome//false
+*/ 
 /*
   Add written explanation of your solution here
+  there is only one iteration through the code
+  the answer is true if the string is palindrome and 
+  false if it isn't palindrome
+  i will use else if
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  // run learn test to check for errors
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +47,13 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting:true");
+  console.log("=>",isPalindrome("abba"));
+
+  console.log("Expecting:true");
+  console.log("=>",isPalindrome("a"));
+
 }
 
 module.exports = isPalindrome;
